a517e84a38d52db24e30939b547968c7c96608c5
cityhospitalreha <city_hospital_reha@icloud.com>
2026-07-08 08:48:09 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyqD5Z24zJ4WUZ1scC6eiul-oUhrkqm6xeOOunTd8cC_CK6i-WyzjKHyYLooSXRcBINHw/exec"
+  GAS_URL: "https://script.google.com/macros/s/AKfycby_6vteCD_p5BRU8b6AGpOuKZFXyKb-ChNVKwfzV4UiTIbR1MdWeH7dUhCjjmlaW7vUTg/exec"
 };
